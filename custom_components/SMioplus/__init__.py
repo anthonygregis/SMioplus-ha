6bd441a193e4843335a18df82f98bd7094d0119f
Anthony Gregis <anthony@gregis.dev>
2024-09-19 16:37:56 -0700

    feat: adds in custom polling solution for faster then 30 second limitation

--- custom_components/SMioplus/__init__.py
+++ custom_components/SMioplus/__init__.py
@@ -2,6 +2,9 @@
 
 import logging
 import voluptuous as vol
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+import asyncio
+from datetime import timedelta
 
 from homeassistant.helpers import config_validation as cv
 from homeassistant.const import (
@@ -20,8 +23,6 @@ CONF_TYPE = "type"
 CONF_CHAN = "chan"
 COM_NOGET = "__NOGET__"
 
-
-
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema(vol.Any([vol.Schema({
         vol.Optional(CONF_STACK, default="0"): cv.string,
@@ -31,11 +32,11 @@ CONFIG_SCHEMA = vol.Schema({
 _LOGGER = logging.getLogger(__name__)
 
 def load_platform(hass, entity_config):
-        for platform_type, attr in SM_MAP.items():
-            if entity_config[CONF_TYPE] in attr:
-                hass.helpers.discovery.load_platform(
-                        platform_type, DOMAIN, entity_config, {}
-                )
+    for platform_type, attr in SM_MAP.items():
+        if entity_config[CONF_TYPE] in attr:
+            hass.helpers.discovery.load_platform(
+                platform_type, DOMAIN, entity_config, {}
+            )
 
 def load_all_platforms(hass, stack=0):
     for platform_type, platform in SM_MAP.items():
@@ -44,15 +45,14 @@ def load_all_platforms(hass, stack=0):
                 continue
             for chan in range(int(attr["chan_no"])):
                 entity_config = {
-                        CONF_NAME: NAME_PREFIX+str(stack)+"_"+type+"_"+str(chan+1),
-                        CONF_STACK: stack,
-                        CONF_TYPE: type,
-                        CONF_CHAN: chan+1
+                    CONF_NAME: NAME_PREFIX+str(stack)+"_"+type+"_"+str(chan+1),
+                    CONF_STACK: stack,
+                    CONF_TYPE: type,
+                    CONF_CHAN: chan+1
                 }
                 hass.helpers.discovery.load_platform(
-                        platform_type, DOMAIN, entity_config, {}
+                    platform_type, DOMAIN, entity_config, {}
                 )
-
 
 def setup(hass, config):
     hass.data[DOMAIN] = []
@@ -74,11 +74,36 @@ def setup(hass, config):
                 continue
             entity_config = card_config[entity] or {}
             entity_config |= {
-                    CONF_NAME: NAME_PREFIX + str(stack) + "_" + entity,
-                    CONF_STACK: stack,
-                    CONF_TYPE: type,
-                    CONF_CHAN: chan
+                CONF_NAME: NAME_PREFIX + str(stack) + "_" + entity,
+                CONF_STACK: stack,
+                CONF_TYPE: type,
+                CONF_CHAN: chan
             }
             load_platform(hass, entity_config)
-        
+    
+    return True
+
+async def async_setup_entry(hass, entry, async_add_entities):
+    async def async_update_data():
+        return None
+
+    coordinator = DataUpdateCoordinator(
+        hass,
+        _LOGGER,
+        name="SMioplus sensors",
+        update_method=async_update_data,
+        update_interval=timedelta(seconds=1),
+    )
+
+    await coordinator.async_config_entry_first_refresh()
+
+    if DOMAIN not in hass.data:
+        hass.data[DOMAIN] = {}
+    hass.data[DOMAIN][entry.entry_id] = {"coordinator": coordinator}
+
+    for platform in SM_MAP:
+        hass.async_create_task(
+            hass.config_entries.async_forward_entry_setup(entry, platform)
+        )
+
     return True
--- custom_components/SMioplus/sensor.py
+++ custom_components/SMioplus/sensor.py
@@ -1,90 +1,54 @@
-DEFAULT_ICONS = {
-        "on": "mdi:numeric",
-        "off": "mdi:numeric-0",
-}
-
-import voluptuous as vol
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
+from homeassistant.components.sensor import SensorEntity
+from homeassistant.const import DEVICE_CLASS_POWER
 import logging
 import time
-import types
-import inspect
-from inspect import signature
 
-import libioplus as SMioplus
-
-from homeassistant.components.light import PLATFORM_SCHEMA
-import homeassistant.helpers.config_validation as cv
-from homeassistant.components.sensor import SensorEntity
-from homeassistant.helpers.entity import generate_entity_id
-
-from . import (
-        DOMAIN, CONF_STACK, CONF_TYPE, CONF_CHAN, CONF_NAME,
-        SM_MAP, SM_API
-)
-SM_MAP = SM_MAP["sensor"]
-
-#SCHEMA_EXTEND = {
-#	vol.Optional(CONF_NAME, default=""): cv.string,
-#	vol.Optional(CONF_STACK, default="0"): cv.string,
-#}
-#for key in SM_SENSOR_MAP:
-#    SCHEMA_EXTEND[vol.Optional(key, default="-1")] = cv.string
-#PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(SCHEMA_EXTEND)
+from . import data
+DOMAIN = data.DOMAIN
+SM_API = data.API
 
 _LOGGER = logging.getLogger(__name__)
 
-def setup_platform(hass, config, add_devices, discovery_info=None):
-    # We want this platform to be setup via discovery
-    if discovery_info == None:
-        return
-    add_devices([Sensor(
-		name=discovery_info.get(CONF_NAME, ""),
-        stack=discovery_info.get(CONF_STACK, 0),
-        type=discovery_info.get(CONF_TYPE),
-        chan=discovery_info.get(CONF_CHAN),
-        hass=hass
-	)])
-
-class Sensor(SensorEntity):
-    def __init__(self, name, stack, type, chan, hass):
-        generated_name = DOMAIN + str(stack) + "_" + type + "_" + str(chan)
-        self._unique_id = generate_entity_id("sensor.{}", generated_name, hass=hass)
-        self._name = name or generated_name
-        self._stack = int(stack)
+class SMioplusSensor(CoordinatorEntity, SensorEntity):
+    def __init__(self, coordinator, SM, stack, chan, type, name, unit, icons):
+        super().__init__(coordinator)
+        self._SM = SM
+        self._stack = stack
+        self._chan = chan
         self._type = type
-        self._chan = int(chan)
-        # Altering class so alln functions have the same format
+        self._name = name
+        self._unit = unit
+        self._icons = icons
+        self._value = None
+        self._icon = icons["off"]
         self._short_timeout = .05
-        self._icons = DEFAULT_ICONS | SM_MAP[self._type].get("icon", {})
-        self._icon = self._icons["off"]
-        self._uom = SM_MAP[self._type]["uom"]
-        self._value = 0
-        self.__SM__init()
+        self._SM_get = getattr(SM, SM_API[type]["get"])
 
-        # Custom setup
-        # I Don't like this hardcoded setup, maybe add a setup com in data.py
-        if self._type == "opto_cnt":
-            self._SM.rstOptoCount(self._stack, self._chan)
-            ## THIS DOESN"T WORK IDK WHY
-            res = self._SM.cfgOptoEdgeCount(self._stack, self._chan, 1)
-            _LOGGER.error(res) # res is 1, so it SHOULD be working
-        ## END
+    @property
+    def name(self):
+        return self._name
 
-    def __SM__init(self):
-        com = SM_MAP[self._type]["com"]
-        self._SM = SM_API
-        if inspect.isclass(self._SM):
-            self._SM = self._SM(self._stack)
-            self._SM_get = getattr(self._SM, com["get"])
-        else:
-            def _aux_SM_get(*args):
-                return getattr(self._SM, com["get"])(self._stack, *args)
-            self._SM_get = _aux_SM_get
+    @property
+    def icon(self):
+        return self._icon
+
+    @property
+    def native_unit_of_measurement(self):
+        return self._unit
+
+    @property
+    def device_class(self):
+        return DEVICE_CLASS_POWER
+
+    @property
+    def native_value(self):
+        self.update()
+        return self._value
 
     def update(self):
         if self._type == "opto_cnt":
             time.sleep(self._short_timeout)
-            ## IT DOESN"T WORK WITHOUT THIS IDK WHY
             self._SM.cfgOptoEdgeCount(self._stack, self._chan, 1)
         time.sleep(self._short_timeout)
         try:
@@ -97,22 +61,16 @@ class Sensor(SensorEntity):
         else:
             self._icon = self._icons["off"]
 
-    @property
-    def unique_id(self):
-        return self._unique_id
-
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def icon(self):
-        return self._icon
-
-    @property
-    def native_unit_of_measurement(self):
-        return self._uom
-
-    @property
-    def native_value(self):
-        return self._value
+async def async_setup_entry(hass, config_entry, async_add_entities):
+    coordinator = hass.data[DOMAIN][config_entry.entry_id]["coordinator"]
+    SM = hass.data[DOMAIN][config_entry.entry_id]["SM"]
+    stack = config_entry.data["stack"]
+    sensors = [
+        SMioplusSensor(coordinator, SM, stack, chan+1, type, 
+                       f"{data.NAME_PREFIX}{stack}_{type}_{chan+1}", 
+                       attr.get("unit", ""),
+                       attr.get("icons", {"on": "mdi:flash", "off": "mdi:flash-off"}))
+        for type, attr in data.SM_MAP["sensor"].items()
+        for chan in range(int(attr["chan_no"]))
+    ]
+    async_add_entities(sensors, True)
